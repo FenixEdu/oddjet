1dae659b86d50613ae2e69fd72c8f032466c0b19
Gil Lacerda <gil.lacerda@ist.utl.pt>
2014-03-18 16:59:21 +0000

    added a logger to the document template as in the FenixReport Class, and set up the template with the default local instead of assuming ENGLISH.

--- src/main/java/org/fenixedu/oddjet/Template.java
+++ src/main/java/org/fenixedu/oddjet/Template.java
@@ -5,15 +5,21 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 abstract public class Template implements Serializable {
 
+    private static final Logger logger = LoggerFactory.getLogger(Template.class);
+
     final private String odtFilePath;
-    private Locale locale = Locale.ENGLISH;
+    private Locale locale;
     final private Map<String, Object> parameters = new HashMap<String, Object>();
     final private Map<String, TableData> tableDataSources = new HashMap<String, TableData>();
 
     public Template(String odtFilePath) {
         this.odtFilePath = odtFilePath;
+        this.locale = Locale.getDefault();
         populate();
     }
 
@@ -53,4 +59,17 @@ abstract public class Template implements Serializable {
 
     abstract protected void populate();
 
+    abstract public String getReportFileName();
+
+    protected void printParameters() {
+        for (Map.Entry<String, Object> entry : parameters.entrySet()) {
+            logger.info(String.format("%s - %s", entry.getKey(), entry.getValue()));
+        }
+    }
+
+    protected void printTableDataSources() {
+        for (Map.Entry<String, Object> entry : parameters.entrySet()) {
+            logger.info(String.format("%s", entry.getKey()));
+        }
+    }
 }
